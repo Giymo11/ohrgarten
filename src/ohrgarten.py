8e01b723f5e2dbd4a936c86063bb400b706c7f63
Gizmo Head <gizmo.head@yahoo.de>
2025-04-25 10:26:48 +0200

    first recordings with arecord

--- src/ohrgarten.py
+++ src/ohrgarten.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 from gpiozero import Button
 from signal import pause
-import time
+import time, signal, subprocess, os
+
+BUTTON_PIN = 17
+ARECORD_CMD = [
+    "arecord",
+    "-D", "plughw:2,0 ",  # find from 'arecord -l'
+    "-f", "cd",       # Record in CD quality (16-bit little endian, 44100 Hz, Stereo)
+    "-t", "wav"       # Save as WAV file type
+]
+
+# Global variable to hold the recording process instance
+recording_process = None
+current_filename = ""
 
 # Use the BCM pin number (GPIO 17, physical pin 11)
-button = Button(17, pull_up=True, bounce_time=0.1)
+button = Button(BUTTON_PIN, pull_up=True, bounce_time=0.1)
 
 def button_pressed():
     print("Button Pressed!")
